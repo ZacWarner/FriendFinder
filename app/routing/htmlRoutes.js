fdae238d70587c1a111b0d10b12783fefb5614c8
ZacWarner <zektgn@gmail.com>
2019-06-13 19:30:32 -0700

    added route for survey, and updated button with link on index.html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,5 +1,9 @@
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
 })
 
 module.exports = htmlRoutes;
