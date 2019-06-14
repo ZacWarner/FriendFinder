485f69f3d31be843bd77a04f506789891b3127fb
ZacWarner <zektgn@gmail.com>
2019-06-13 18:57:35 -0700

    set up server.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,3 +1,5 @@
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 })
+
+module.exports = htmlRoutes;
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require("express");
 var path = require("path");
 
+
+
+
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+var apiRoutes = require("./app/routing/apiRoutes");
+var htmlRoutes = require("./app/routing/htmlRoutes");
+
+app.use(apiRoutes);
+app.use(htmlRoutes);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
